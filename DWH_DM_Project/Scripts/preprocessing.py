e7a14ecb3189f67fd6b3a74d90fc26d2b2e81e69
Ahmed Tidiane BALDE <baldeahmedtidiane36@gmail.com>
2018-05-12 09:02:46 +0200

    Avance petit à petit

--- DWH_DM_Project/Scripts/preprocessing.py
+++ DWH_DM_Project/Scripts/preprocessing.py
@@ -1,47 +1,50 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 df_mig = pd.read_csv("../Initial Data/MIG.csv", encoding="utf-8")
+
 
 def get_columns(listcolumns, dataframe, index=False):
     """ Returns a list of columns of the given dataframe
 
-    :param listcolumns: 
-    :param dataframe: 
-    :returns: 
-    :rtype: 
+    :param listcolumns:
+    :param dataframe:
+    :returns:
+    :rtype:
 
     """
 
     if index:
-        return dataframe.loc[:, listcolumns]
-    else:
         return dataframe.iloc[:, listcolumns]
+    else:
+        return dataframe.loc[:, listcolumns]
+
 
 def create_features(value, column, pos=True):
     """
 
-    :param value: 
-    :param column: 
-    :param pos: 
-    :returns: 
-    :rtype: 
+    :param value:
+    :param column:
+    :param pos:
+    :returns:
+    :rtype:
 
     """
-    
+
     if pos:
-        concat = lambda x: value + x
+        def concat(x): return value + x
     else:
-        concat = lambda x: x + value
-        
+        def concat(x): return x + value
+
     return map(concat, column)
 
 
-if __name__=="__main__":
-    code_pays = get_columns([0], df_mig)
+if __name__ == "__main__":
+    code_pays = get_columns(['COU'], df_mig)
     all_countries = np.unique(code_pays.values.ravel())
     features_pays = create_features('immigres_', all_countries)
-    features_pays = create_features(' bigint NOT NULL,', features_pays, pos=False)
+    features_pays = create_features(
+        ' bigint NOT NULL,', features_pays, pos=False)
     print("\n".join(map(str, features_pays)))
